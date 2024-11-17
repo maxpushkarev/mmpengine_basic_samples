d42abceecb8680ba64ce617de8218e954f7b9402
Максим Пушкарев <maxim.pushkarev@playrix.com>
2024-11-17 16:36:45 +0400

    fixed headers

--- src/samples/Cube/Main.cpp
+++ src/samples/Cube/Main.cpp
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include <Core/Context.hpp>
 #ifdef MMPENGINE_WIN
 #include <Windows.h>
 #endif
@@ -7,6 +8,15 @@
 std::int32_t WINAPI WinMain(HINSTANCE hInstance, HINSTANCE,
 	PSTR, std::int32_t)
 {
+	MMPEngine::Core::AppContextSettings appContextSettings {};
+#if (defined(DEBUG) || defined(_DEBUG)) 
+	appContextSettings.isDebug = true;
+#else
+	appContextSettings.isDebug = false;
+#endif
+
+	appContextSettings.backend = MMPEngine::Core::BackendType::Dx12;
+
 	return 0;
 }
 #endif
