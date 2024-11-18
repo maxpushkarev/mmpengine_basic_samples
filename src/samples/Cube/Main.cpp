bf3264f7028834353ef03579f8ec7169251e7080
Максим Пушкарев <maxim.pushkarev@playrix.com>
2024-11-18 17:43:03 +0400

    added: logger

--- src/samples/Cube/Main.cpp
+++ src/samples/Cube/Main.cpp
@@ -1,6 +1,7 @@
 #include <cstdint>
 #include <Core/Context.hpp>
 #include <Frontend/App.hpp>
+#include <Frontend/Logger.hpp>
 #include <Cube/App.hpp>
 #ifdef MMPENGINE_WIN
 #include <Windows.h>
@@ -21,6 +22,7 @@ std::int32_t WINAPI WinMain(HINSTANCE hInstance, HINSTANCE,
 
 	const auto userApp = std::make_shared<Cube::App>();
 	const auto rootApp = MMPEngine::Frontend::App::BuildRootApp(appContextSettings, userApp);
+	rootApp->GetContext()->logger = std::make_unique<MMPEngine::Frontend::Win::OutputLogger>("Cube sample logger");
 
 	return 0;
 }
