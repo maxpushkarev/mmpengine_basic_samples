ed43b99ccb714c19e81ec85bf14527fccbfca26d
Максим Пушкарев <maxim.pushkarev@playrix.com>
2024-12-08 21:17:53 +0400

    compute shader in dx12 and frontene

--- src/samples/Compute/App.cpp
+++ src/samples/Compute/App.cpp
@@ -1,6 +1,7 @@
 #include <Frontend/App.hpp>
 #include <Compute/App.hpp>
 #include <Frontend/Shader.hpp>
+#include <Frontend/Material.hpp>
 
 namespace Sample::Compute
 {
@@ -16,11 +17,17 @@ namespace Sample::Compute
 	void App::Test_SetThreadId()
 	{
 		const auto computeShader = MMPEngine::Frontend::Shader::LoadFromFile<MMPEngine::Core::ComputeShader>(GetContext(), std::filesystem::path("Compute_SetThreadId.json"));
+		const auto material = std::make_shared<MMPEngine::Frontend::ComputeMaterial>(GetContext(), computeShader);
+
 		const auto stream = GetDefaultStream();
 
 		{
 			const auto executor = stream->CreateExecutor();
 			stream->Schedule(computeShader->CreateInitializationTask());
+			stream->Schedule(material->CreateInitializationTask());
+
+			MMPEngine::Core::BaseMaterial::Parameters params { std::vector<MMPEngine::Core::BaseMaterial::Parameters::Entry> {} };
+			stream->Schedule(material->CreateTaskForUpdateParameters(std::move(params)));
 		}
 	}
 
