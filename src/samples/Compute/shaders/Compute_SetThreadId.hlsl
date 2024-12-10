ba452ac59e4095ade5cf3885486adc775e23ea9f
Максим Пушкарев <maxim.pushkarev@playrix.com>
2024-12-11 00:23:22 +0400

    bckp

--- src/samples/Compute/shaders/Compute_SetThreadId.hlsl
+++ src/samples/Compute/shaders/Compute_SetThreadId.hlsl
@@ -1,4 +1,8 @@
-[numthreads(1, 1, 1)]
+#include "../../../../libs/mmpengine/src/Backend/Dx12/Shader.h"
+RWStructuredBuffer<uint> output : register(u0);
+
+[numthreads(128, 1, 1)]
 void EntryPoint(uint3 tId : SV_DispatchThreadId)
 {
+	output[tId.x] = tId.x;
 }
