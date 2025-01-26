415acbe71c46b5917b69527f08e877f40ad24896
Максим Пушкарев <155516836+pushkarev-m@users.noreply.github.com>
2025-01-26 22:09:06 +0400

    bckp

--- src/samples/Compute/Main.cpp
+++ src/samples/Compute/Main.cpp
@@ -6,5 +6,6 @@ std::int32_t WINAPI WinMain(HINSTANCE hInstance, HINSTANCE,
 	PSTR, std::int32_t)
 {
 	return MainTemplate::WinMain<Sample::Compute::App>(hInstance, MMPEngine::Core::BackendType::Dx12, "Compute");
+	//return MainTemplate::SharedMain<Sample::Compute::App>(MMPEngine::Core::BackendType::Dx12, "Compute");
 }
 #endif
