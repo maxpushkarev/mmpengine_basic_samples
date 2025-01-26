c2f9159d1fb2fa5a1a94b299de4e52c92ec86ada
Максим Пушкарев <155516836+pushkarev-m@users.noreply.github.com>
2025-01-26 21:31:41 +0400

    added: glfw app container

--- src/samples/MainTemplate.hpp
+++ src/samples/MainTemplate.hpp
@@ -18,7 +18,44 @@ public:
 	static std::int32_t WinMain(HINSTANCE hInstance, MMPEngine::Core::BackendType backendType, const std::string& userAppName, std::optional<bool> isDebug = std::nullopt);
 #endif
 
+	template<typename TUserApp>
+	static std::int32_t SharedMain(MMPEngine::Core::BackendType backendType, const std::string& userAppName, std::optional<bool> isDebug = std::nullopt);
 };
+
+template<typename TUserApp>
+inline std::int32_t MainTemplate::SharedMain(MMPEngine::Core::BackendType backendType, const std::string& userAppName, std::optional<bool> isDebug)
+{
+	MMPEngine::Core::GlobalContext::Settings appContextSettings {};
+
+	if (isDebug.has_value())
+	{
+		appContextSettings.isDebug = isDebug.value();
+	}
+	else
+	{
+#if (defined(DEBUG) || defined(_DEBUG)) 
+		appContextSettings.isDebug = true;
+#else
+		appContextSettings.isDebug = false;
+#endif
+	}
+
+	appContextSettings.backend = backendType;
+
+#ifdef MMPENGINE_WIN
+	const auto logger = std::make_shared<MMPEngine::Feature::Win::OutputLogger>(userAppName + " logger");
+#endif
+	auto userApp = std::make_unique<TUserApp>(logger);
+	auto rootApp = MMPEngine::Feature::App::BuildRootApp(appContextSettings, std::move(userApp), nullptr, logger);
+
+	MMPEngine::Feature::Shared::AppContainer::Settings appContainerSettings {};
+	appContainerSettings.base.targetFps = 360;
+	appContainerSettings.base = { userAppName };
+
+	const auto appContainer = std::make_unique<MMPEngine::Feature::Shared::AppContainer>(std::move(appContainerSettings), std::move(rootApp));
+	return appContainer->Run();
+	return 0;
+}
 
 #ifdef MMPENGINE_WIN
 
