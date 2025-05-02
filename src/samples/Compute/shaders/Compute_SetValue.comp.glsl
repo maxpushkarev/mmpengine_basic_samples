eb6ee9a636f30adfe64d9251b31783e353f9ba18
Максим Пушкарев <155516836+pushkarev-m@users.noreply.github.com>
2025-05-02 21:38:35 +0400

    set value shader glsl

--- src/samples/Compute/shaders/Compute_SetValue.comp.glsl
+++ src/samples/Compute/shaders/Compute_SetValue.comp.glsl
@@ -1,22 +1,17 @@
 #version 450
 
-layout(set = 0, binding = 0) uniform GlobalUniforms {
-    uint numElements;
-    float someGlobalValue;
-} ubo;
-
-layout(set = 1, binding = 0) buffer InputBuffer {
-    float inputData[];
+layout(set = 0, binding = 0) buffer Buffer {
+    uint outputData[];
 };
 
-layout(set = 1, binding = 1) buffer OutputBuffer {
-    float outputData[];
-};
+layout(set = 1, binding = 0) uniform GlobalUniforms0 {
+    uint multiplier;
+} ubo0;
+
+layout(set = 1, binding = 1) uniform GlobalUniforms1 {
+    uint addition;
+} ubo1;
 
 void main() {
-    uint idx = gl_GlobalInvocationID.x;
-    if (idx >= ubo.numElements) {
-        return;
-    }
-    outputData[idx] = inputData[idx] * ubo.someGlobalValue;
+	outputData[gl_GlobalInvocationID.x] = gl_GlobalInvocationID.x * ubo0.multiplier + ubo1.addition;
 }
