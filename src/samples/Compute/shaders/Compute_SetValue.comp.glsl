e98cbaf44a2c6bad5fff56702f459222f6a074e2
Максим Пушкарев <155516836+pushkarev-m@users.noreply.github.com>
2025-05-03 17:04:33 +0400

    fixed: std140 and std430

--- src/samples/Compute/shaders/Compute_SetValue.comp.glsl
+++ src/samples/Compute/shaders/Compute_SetValue.comp.glsl
@@ -1,17 +1,18 @@
 #version 450
 
-layout(set = 0, binding = 0) buffer Buffer {
+layout(std430, set = 0, binding = 0) buffer Buffer {
     uint outputData[];
 };
 
-layout(set = 1, binding = 0) uniform GlobalUniforms0 {
+layout(std140, set = 1, binding = 0) uniform GlobalUniforms0 {
     uint multiplier;
 } ubo0;
 
-layout(set = 1, binding = 1) uniform GlobalUniforms1 {
+layout(std140, set = 1, binding = 1) uniform GlobalUniforms1 {
     uint addition;
 } ubo1;
 
+layout (local_size_x = 128, local_size_y = 1, local_size_z = 1) in;
 void main() {
 	outputData[gl_GlobalInvocationID.x] = gl_GlobalInvocationID.x * ubo0.multiplier + ubo1.addition;
 }
