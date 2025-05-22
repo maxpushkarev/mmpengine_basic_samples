cd23fa37f99b166ae14b52f61fadd718ffd6fd31
Максим Пушкарев <155516836+pushkarev-m@users.noreply.github.com>
2025-05-22 07:53:18 +0400

    fixed: boxes

--- src/samples/Boxes/Main.cpp
+++ src/samples/Boxes/Main.cpp
@@ -5,7 +5,7 @@
 std::int32_t WINAPI WinMain(HINSTANCE hInstance, HINSTANCE,
 	PSTR, std::int32_t)
 {
-	return MainTemplate::WinMain<Sample::Boxes::App>(hInstance, MMPEngine::Core::BackendType::Dx12, "Boxes");
-	//return MainTemplate::SharedMain<Sample::Boxes::App>(MMPEngine::Core::BackendType::Dx12, "Boxes");
+	//return MainTemplate::WinMain<Sample::Boxes::App>(hInstance, MMPEngine::Core::BackendType::Dx12, "Boxes");
+	return MainTemplate::SharedMain<Sample::Boxes::App>(MMPEngine::Core::BackendType::Vulkan, "Boxes");
 }
 #endif
