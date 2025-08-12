32640d3e2e31fe6c1139041a49ddd6eea2217715
Max Pushkarev <pushkarevmm@gmail.com>
2025-08-12 22:37:35 +0400

    uncomment tests for structured buffer and uniform buffer in metal

--- src/samples/Buffers/App.cpp
+++ src/samples/Buffers/App.cpp
@@ -13,10 +13,10 @@ namespace Sample::Buffers
 		UserApp::Initialize();
 
 		Test_Upload_To_Resident_To_ReadBack();
-		/*Test_UniformBufferTo_ReadBack();
+		Test_UniformBufferTo_ReadBack();
 		Test_Structured_Buffers();
-		Test_IndexBuffer();
-		Test_VertexBuffer();*/
+		//Test_IndexBuffer();
+		//Test_VertexBuffer();
 	}
 
 	void App::Test_Upload_To_Resident_To_ReadBack()
